b70a1461a6ba6f36fc24061eb7001ea8a31eefa5
Talles Augusto Passos Siscoutto <125299678+Talles10@users.noreply.github.com>
2025-04-08 22:20:57 -0300

    Update web.php

--- projetobanco/routes/web.php
+++ projetobanco/routes/web.php
@@ -6,17 +6,6 @@ use App\Http\Controllers\RF_B02Controller;
 use App\Http\Controllers\RF_B03Controller;
 use App\Http\Controllers\RF_B04Controller;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
